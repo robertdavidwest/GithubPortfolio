110e32f423c21101c0796b15a3d8cc5d77b702e3
Robert West <robert.david.west@gmail.com>
2023-03-04 11:24:14 -0500

    comment

--- src/tools/github-data/main.ts
+++ src/tools/github-data/main.ts
@@ -1,13 +1,16 @@
 import {getReadmeData} from "./readmeData";
 
 export function getAllGithubData(username: string) {
+  // Add all function calls here to collect all github data 
+  // in one place: 
   return getReadmeData(username);
 }
 
 async function main() {
-  // test run this fns for my username: 
+  // test run this fn for my username: 
   const data = await getAllGithubData('robertdavidwest');
   console.log(data);
 }
 
-main();
+// for testing : 
+//main();
