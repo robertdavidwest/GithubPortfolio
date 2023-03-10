1555bfe868a359e6f81821f97f75392edf6dfc5f
Robert West <robert.david.west@gmail.com>
2023-03-10 12:10:09 -0500

    reading portfolio img from repo

--- src/data/github-data/starredRepoData.ts
+++ src/data/github-data/starredRepoData.ts
@@ -1,7 +1,6 @@
 import {Octokit} from '@octokit/rest';
 
 import {PortfolioItem} from '../dataDef';
-import {getRawTaggedData} from './readmeData';
 
 const octokit = new Octokit();
 
@@ -9,28 +8,14 @@ async function requestStarredRepoData(username: string) {
   const {data} = await octokit.rest.activity.listReposStarredByUser({
     username,
   });
-
   return data;
 }
 
-async function requestStarredRepoReadMe(username: string, repoName: string) {
-  const {data} = await octokit.rest.repos.getContent({
-    owner: username,
-    repo: repoName,
-    path: 'README.md',
-  });
-  if("content" in data){
-    const content: string = Buffer.from(data.content, 'base64').toString();
-    return content;
-  } else return "";
-}
-
-async function formatPortfolioItems(data: any[], username: string) {
+async function formatPortfolioItems(data: any[]) {
   const portfolioItems: PortfolioItem[] = [];
 
-  for (let repo of data) {
-    const readmeData = await requestStarredRepoReadMe(username, repo.name)
-    const imgUrl = getRawTaggedData(readmeData, "imgUrl");
+  for (const repo of data) {
+    const imgUrl = `https://raw.githubusercontent.com/${repo.owner.login}/${repo.name}/HEAD/app-screenshot.png`
 
     const portfolioItemData: PortfolioItem = {
       title: repo.name,
@@ -46,7 +31,7 @@ async function formatPortfolioItems(data: any[], username: string) {
 
 export async function getStarredRepoData(username: string) {
   const data = await requestStarredRepoData(username);
-  const portfolioItems: PortfolioItem[] = await formatPortfolioItems(data, username);
+  const portfolioItems: PortfolioItem[] = await formatPortfolioItems(data);
 
   return portfolioItems;
 }
